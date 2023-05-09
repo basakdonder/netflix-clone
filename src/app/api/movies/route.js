9a21524519604b8809391b5133d023cea6c39dda
Başak Dönder <basakdonder01@gmail.com>
2023-05-10 02:56:02 +0300

    api test

---
+++ src/app/api/movies/route.js
@@ -0,0 +1,8 @@
+const { NextResponse } = require("next/server");
+
+export async function GET() {
+    const res = await fetch(`${process.env.API_ENDPOINT}/movie/popular/?api_key=${process.env.API_KEY}`);
+    const data = await res.json()
+
+    return NextResponse.json({data})
+}
