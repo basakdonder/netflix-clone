3306787003eb5e762cfe940d2e918977bfc5c37e
Başak Dönder <basakdonder01@gmail.com>
2023-05-30 21:23:31 +0300

    favicon and font update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,22 @@
 import "./assets/css/globals.css"
 import "./assets/css/tw.scss"
+import { Inter } from 'next/font/google';
 import Header from "./components/header/layout"
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: "Netflix Clone",
   description: "Netflix Clone coded with Next.js 13 and TMDB API",
   icons: {
-    icon: "./assets/img/favicon.ico",
-  },
+    icon:"/favicon.ico",
+  }
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <Header />
 
         <section>{children}</section>
