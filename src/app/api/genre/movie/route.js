d7cc2802dc4cedca848f90ff63eacd5bbbb55aaf
Başak Dönder <basakdonder01@gmail.com>
2023-06-09 22:06:12 +0300

    api urls updated

--- src/app/api/genre/movie/route.js
+++ src/app/api/genre/movie/route.js
@@ -2,7 +2,7 @@ const { NextResponse } = require("next/server")
 
 export async function GET() {
   const res = await fetch(
-    `${process.env.API_ENDPOINT}/genre/movie/list?api_key=${process.env.API_KEY}`,
+    `${process.env.NEXT_PUBLIC_API_ENDPOINT}/genre/movie/list?api_key=${process.env.NEXT_PUBLIC_API_KEY}`,
   )
   const data = await res.json()
 
